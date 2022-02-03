574ba4d09c820a05ea4e1b15ed306f37b9936135
GhersiA <93844255+GhersiA@users.noreply.github.com>
2022-02-02 22:00:40 -0500

    validation

--- index.js
+++ index.js
@@ -20,5 +20,79 @@ window.addEventListener("beforeinstallprompt", function(e) {
 
   // The event was re-dispatched in response to our request
   // ...
+//});
+//Tosty();
+//let deferrdPrompt;
+//window.addEventListener('beforeinstallprompt', function(e) {
+
+//  e.preventDefault();
+
+ // deferrdPrompt = e.prompt;
+ // console.log(`'deferrdPrompt' event was fired.`);
+
+  //const installAppButton = document.getElementById('installAppButton');
+  //f (installAppButton){
+  //  installAppButton.addEventListener('click',function(){
+   //   deferrdPrompt.prompt();
+   // })
+ // }
+//});
+//var option = {
+ // animation:true, 
+
+ // delay: 4000
+
+//};
+
+//window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  //e.preventDefault();
+
+  //let deferredPrompt;
+
+  // Stash the event so it can be triggered later.
+ //deferredPrompt = e;
+  // Show the prompt
+ // deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+//  deferredPrompt.userChoice
+  //.then((choiceResult) => {
+  //  if (choiceResult.outcome === 'accepted') {
+     // console.log('User accepted the A2HS prompt');
+   // } else {
+     // console.log('User dismissed the A2HS prompt');
+  //  }
+   // deferredPrompt = null;
+ /// })
+//})
 });
-document.getElementById("installer l'application ? ").innerHTML = resultatPrompt;
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log('service worker not registered', err));
+}
+
+
+
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function(event) {
+alert('beforeinstallprompt fired');
+event.preventDefault();
+deferredPrompt = event;
+return false;
+});
+
+function addToHomeScreen() {
+if (deferredPrompt) {
+deferredPrompt.prompt();
+deferredPrompt.userChoice.then(function (choiceResult) {
+console.log(choiceResult.outcome);
+if (choiceResult.outcome === 'dismissed') {
+	console.log('User cancelled installation');
+} else {
+	console.log('User added to home screen');
+}
+});
+deferredPrompt = null;
+}
+}
