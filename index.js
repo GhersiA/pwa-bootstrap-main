53dd6c41afe624f7a15bdce99ed72368adc68473
GhersiA <93844255+GhersiA@users.noreply.github.com>
2022-02-16 23:12:53 -0500

    ajouter page hors ligne avec un message

--- index.js
+++ index.js
@@ -18,53 +18,6 @@ window.addEventListener("beforeinstallprompt", function(e) {
     }, 10000);
   }
 
-  // The event was re-dispatched in response to our request
-  // ...
-//});
-//Tosty();
-//let deferrdPrompt;
-//window.addEventListener('beforeinstallprompt', function(e) {
-
-//  e.preventDefault();
-
- // deferrdPrompt = e.prompt;
- // console.log(`'deferrdPrompt' event was fired.`);
-
-  //const installAppButton = document.getElementById('installAppButton');
-  //f (installAppButton){
-  //  installAppButton.addEventListener('click',function(){
-   //   deferrdPrompt.prompt();
-   // })
- // }
-//});
-//var option = {
- // animation:true, 
-
- // delay: 4000
-
-//};
-
-//window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  //e.preventDefault();
-
-  //let deferredPrompt;
-
-  // Stash the event so it can be triggered later.
- //deferredPrompt = e;
-  // Show the prompt
- // deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-//  deferredPrompt.userChoice
-  //.then((choiceResult) => {
-  //  if (choiceResult.outcome === 'accepted') {
-     // console.log('User accepted the A2HS prompt');
-   // } else {
-     // console.log('User dismissed the A2HS prompt');
-  //  }
-   // deferredPrompt = null;
- /// })
-//})
 });
 if('serviceWorker' in navigator){
   navigator.serviceWorker.register('/sw.js')
--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ self.addEventListener("install", event=>{
     caches.open(["precache-v1"]).then(cache=>{
       cache.addAll(["/", "index.js",
          "index.html",
+         "page-hors-ligne.html",
          "bootstrap-5.1.3-dist/css/bootstrap.min.css",
         "icons-1.7.2/font/bootstrap-icons.css",
         "bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js",
@@ -11,10 +12,25 @@ self.addEventListener("install", event=>{
   );
 });
 
-self.addEventListener("fetch", event=>{
-  event.respondWith(
-    caches.match(event.request).then(response=>{
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function (event) {
+  console.log("Fetching ..." + event.request.url);
+  event.respondWith(cacheOrNetwork(event.request).catch(() => fallbackVersPageHorsLigne()));
 });
+
+async function cacheOrNetwork(request) {
+  try {
+    return await fromCache(request);
+  } catch {
+    return await fetch(request);
+  }
+};
+
+async function fromCache(request) {
+  const cache = await caches.open('v1');
+  const matching = await cache.match(request);
+  return matching || Promise.reject('no-match');
+}
+
+function fallbackVersPageHorsLigne() {
+  return caches.match("page-hors-ligne.html");
+}
